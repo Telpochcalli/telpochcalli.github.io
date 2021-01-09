bbd48bbe8e9103e8b37c848090fbb8ffbc437cf3
Luis Fernández <a01023675@itesm.mx>
2021-01-09 15:31:16 -0600

    First commit, half index

---
+++ js/replicable.js
@@ -0,0 +1,36 @@
+// Navigation Data
+var navigationData = [{
+      title: "Robomaster",
+      href: "robomaster.html"
+    },
+    {
+      title: "About Us",
+      href: "about-us.html"
+    },
+    {
+      title: "Blog",
+      href: "blog.html"
+    },
+    {
+      title: "Contact Us",
+      href: "contact.html"
+    }]
+//
+function loadReplicables() {
+    var navigations = document.getElementsByClassName('navigation');
+    for (var i = 0; i < navigations.length; i++) {
+        loadNavigation(navigations[i]);
+    }
+}
+//
+function loadNavigation(navigation) {
+    navigationData.forEach((item, i) => {
+        var newA = document.createElement("a");
+        newA.innerHTML = item.title;
+        newA.href = item.href;
+        newA.target = "_self";
+        navigation.appendChild(newA);
+    });
+}
+
+window.onload = loadReplicables;
